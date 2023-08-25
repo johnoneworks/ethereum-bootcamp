6253266434c9d719e0010566f4bf8cd4b96360f8
john.huang <john.huang@onelab.tw>
2023-08-25 17:52:16 +0800

    exercise-2

---
+++ week-1/exercise-2/getAddress.js
@@ -0,0 +1,8 @@
+const { secp2561 } = require("ethereum-cryptography/secp256k1");
+const { keccak256 } = require("ethereum-cryptography/keccak");
+
+function getAddress(publicKey) {
+    // function 內容就靠妳了
+}
+
+module.exports = getAddress;
